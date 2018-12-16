28f5b7822dd3d060603069e698185d37b412300e
Jonathan Harlap <jharlap@gmail.com>
2018-12-16 00:13:42 +0000

    day 15 wip2

--- day15/dijkstra.go
+++ day15/dijkstra.go
@@ -5,6 +5,40 @@ import (
 	"fmt"
 )
 
+func demo() {
+	// Some items and their priorities.
+	items := map[string]int{
+		"banana": 3, "apple": 2, "pear": 4,
+	}
+
+	// Create a priority queue, put the items in it, and
+	// establish the priority queue (heap) invariants.
+	pq := make(PriorityQueue, len(items))
+	i := 0
+	for value, priority := range items {
+		pq[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
+	}
+	heap.Init(&pq)
+
+	// Insert a new item
+	item := &Item{
+		value:    "orange",
+		priority: 1,
+	}
+	heap.Push(&pq, item)
+
+	// Take the items out; they arrive in decreasing priority order.
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*Item)
+		fmt.Printf("%.2d:%s ", item.priority, item.value)
+	}
+}
+
 // An Item is something we manage in a priority queue.
 type Item struct {
 	value    string // The value of the item; arbitrary.
